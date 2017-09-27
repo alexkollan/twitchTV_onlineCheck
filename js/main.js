81d6473a75af82a6605f8e8d57dfc4a203c957fd
Alex Collin <finapocalypse@gmail.com>
2017-09-27 06:01:23 +0300

    Fixed the problem which caused offline channels to not display anything. Added some preliminary styling to help developement. Still in heavy developement. This is for fun so it might take long! :P

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
-const twtchAPI = 'https://cors-anywhere.herokuapp.com/https://wind-bow.gomix.me/twitch-api/streams/';
+const twitchAPISTREAMS = 'https://cors-anywhere.herokuapp.com/https://wind-bow.gomix.me/twitch-api/streams/';
+const twitchAPICHANNELS = 'https://cors-anywhere.herokuapp.com/https://wind-bow.gomix.me/twitch-api/channels/'
+//dyrus (30080751), wingsofdeath (30171560), nightblue3, imaqtpie, 
+//frogen, shiptur, biasfate, yassuo, tobiasfate
 
+var pre_streams = ["ESL_SC2", "OgamingSC2", "trick2g", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
+var isOnline;
+var noLogo = 'https://www.twitch.tv/p/assets/uploads/glitch_notcomboone_474x356.png';
 
 $('#srchBtn').on('click',takeInput);
 $(document).on('keypress',function(e) {
@@ -7,8 +13,41 @@ $(document).on('keypress',function(e) {
         takeInput();
     }
 });
-function fetchData(streamer){
-    fetch(twtchAPI+streamer,{
+function fetchDataStream(streamer){
+    fetch(twitchAPISTREAMS+streamer,{
+        headers : { 
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+       }
+    })
+    .then(function(response){
+        console.log(response.status);
+        response.json().then(function(data){
+            
+            if(data.stream == null){
+                console.log('offline');
+                isOnline = false;
+                fetchDataChannel(streamer);
+            }else{
+                console.log('online');
+                isOnline = true;
+                drawResult(data.stream.preview.medium,data.stream.channel.display_name,data.stream.stream_type,data.stream.game,data.stream.viewers,data.stream.average_fps,data.stream.channel.url);
+            }
+            console.log(data);
+            
+
+        }).catch(function(err){
+            console.log(err);
+        })
+    })
+}
+
+
+//================================================================
+
+
+function fetchDataChannel(streamer){
+    fetch(twitchAPICHANNELS+streamer,{
         headers : { 
         'Content-Type': 'application/json',
         'Accept': 'application/json'
@@ -18,33 +57,76 @@ function fetchData(streamer){
         console.log(response.status);
         response.json().then(function(data){
             console.log(data);
-            drawResult(data.stream.preview.medium,data.stream.channel.display_name,data.stream.stream_type,data.stream.game,data.stream.viewers,data.stream.average_fps);
+            drawResultC(data.profile_banner, data.display_name, data.status, data.updated_at, data.url);
+
         }).catch(function(err){
             console.log(err);
         })
     })
+    console.log('THIS IS DA CHANNELZ');
 }
-function drawResult(preview,name,status,playing,viewers,fps){
+function drawResult(preview,name,status,playing,viewers,fps,channelURL){
 
     $('.responseBox').append(
         `
-        <img src="${preview}">
-        <h4>Streamer: </h4><h5>${name}</h5>
-        <h4>Channel is </h4><h5>${status}</h5>
-        <h4>Playing: </h4><h5>${playing}</h5>
-        <h4>Viewers: </h4><h5>${viewers}</h5>
-        <h4>FPS: </h4><h5>${fps}</h5>
-        
+        <div class="card streams" style="width: 20rem;">
+        <img class="card-img-top logo" src="${preview}" alt="Card image cap">
+        <div class="card-block">
+          <h4 class="card-title">${name}</h4>
+          <p class="card-text">
+          Channel is ${status}
+          playing ${playing}
+          <br>Viewers : ${viewers}
+          <br>FPS: ${fps}
+          <p>Status ${isOnline}</p
+          </p>
+          <a href="${channelURL}" target="_blank"><div id="goToBtn" class="btn btn-primary">Go to Channel</div></a>
+        </div>
+      </div>
         `
     )
+    $('#goToBtn').on('click', function() {
+        // window.open(channelURL);
+    })
 }
+
+function drawResultC(banner,name,status,statusUpdateDate,channelURL){
+    let image;
+    if(banner == null){
+        image = noLogo;
+    }else{
+        image = banner;
+    }
+        $('.responseBox').append(
+            `
+            <div class="card streams" style="width: 20rem;">
+            <img class="card-img-top logo" src="${image}" alt="Card image cap">
+            <div class="card-block">
+              <h4 class="card-title">${name} </h4> 
+              <p class="card-text">
+              <i>is Offline</i>
+              </p>
+              <a href="${channelURL}" target="_blank"><div id="goToBtn" class="btn btn-primary">Go to Channel</div></a>
+            </div>
+          </div>
+            `
+        )
+        $('#goToBtn').on('click', function() {
+            // window.open(channelURL);
+        })
+    }
+
 function takeInput(){
     var name = $('#streamer').val();
     console.log(name);
-    if (name == '') {
-        alert(`Please don't leave me empty... :(`)
-    } else {
-        fetchData(name);
-    }
+    // if (name == '') {
+    //     alert(`Please don't leave me empty... :(`)
+    // } else {
+        pre_streams.forEach(function(name){
+            fetchDataStream(name);
+
+        })
+        
+    // }
 
 }
